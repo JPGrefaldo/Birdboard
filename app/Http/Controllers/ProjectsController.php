5c579fb00958144838ab566bd6303fd9e78f94ee
JPGrefaldo <johnpaulgrefaldo@gail.com>
2019-02-28 14:16:58 +0800

    Scoping Projects

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -9,22 +9,25 @@ class ProjectsController extends Controller
 {
     public function index()
     {
-        $projects = \App\Project::all();
+        $projects = auth()->user()->projects;
 
         return view('projects.index', compact('projects'));
     }
 
     public function store()
     {   
-        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
-
-        \App\Project::create($attributes);
+        auth()->user()->projects()->create(request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            ])
+        );
 
         return redirect('/projects');
     }
 
     public function show(Project $project)
     {
+        abort_if(auth()->user()->isNot($project->owner),  403);
         return view('projects.show', compact('project'));
     }
 }
